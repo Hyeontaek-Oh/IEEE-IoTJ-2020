8d4a44075a3ea756fc04cabbaa92a7dd338f3975
Hyeontaek-Oh <hyeontaek.oh@gmail.com>
2020-01-22 12:33:47 +0900

    Add comments for explaining each condition

--- slsqp-broker-opt-example.py
+++ slsqp-broker-opt-example.py
@@ -1,6 +1,6 @@
 # IEEE Internet of Things Journal
 # Submitted manuscript IoT-8527-2019.R2
-# An example of SLSQP application for data broker cost minimization
+# An example of SLSQP application for data broker cost minimization (Problem 2)
 # Author: Hyeontaek Oh (hyeontaek@kaist.ac.kr)
 
 import numpy as np
@@ -14,19 +14,19 @@ rho = [ [0.01, 0.05], [0.01, 0.05] ]
 
 i = 0
 ci = [0.0] * K # target vector c
-# objective function: cost minimization
+# objective function: cost minimization (equation (20) in Problem 2)
 def min_cost(ci):
   E = 0.0
   for k in range(K):
       E = E + ci[k]
   return E
 
-# set boundary conditions for c
+# set boundary conditions for c (condition (21) in Problem 2)
 bnds = ((0.0, 2.0/rho[i][0]),)
 for k in range(1, K):
   bnds = bnds + ((0.0, 2.0/rho[i][k]),)
 
-# equality constraint
+# equality constraint (condition (22) in Problem 2)
 def broker_q_cons(ci):
   q = 0.0
   for x in range(K):
@@ -36,10 +36,12 @@ def broker_q_cons(ci):
                        * np.sqrt((1-np.exp(-1.0*ci[x]*rho[i][x])) \
                                 *(1-np.exp(-1.0*ci[y]*rho[i][y])))
   return q_star[i] - q
-    
+
+# set pre-defined conditions for SLSQP solver
 con1 = {'type': 'eq', 'fun': broker_q_cons}
 cons = [con1]
 
+# Run SLSQP solver
 solution = minimize(min_cost, ci, method='SLSQP', \
                     bounds=bnds, constraints=cons)
 
